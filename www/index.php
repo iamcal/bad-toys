7b32075c9574ec0e08b3acefe3829d33d7d7299d
Cal Henderson <cal@iamcal.com>
2012-08-01 17:35:29 -0700

    show index age on homepage

--- www/index.php
+++ www/index.php
@@ -2,8 +2,11 @@
 	include('include/init.php');
 
 
-	$rows = array();
+	#
+	# fetch errors
+	#
 
+	$rows = array();
 
 	$order = $_GET['recent'] ? 'date_latest DESC' : 'num_logged DESC';
 
@@ -16,6 +19,33 @@
 	}
 
 	$smarty->assign('rows', $rows);
+
+
+	#
+	# index age
+	#
+
+	list($latest) = db_list(db_fetch("SELECT date_logged FROM js_errors_events ORDER BY date_logged DESC LIMIT 1"));
+	$age = time() - $latest;
+
+	if ($age < 5){
+		$ago = "just now";
+	}elseif ($age < 90){
+		$ago = "$age seconds ago";
+	}elseif ($age < 90*60){
+		$m = round($age/60);
+		$ago = "$m minutes ago";
+	}else{
+		$h = round($age/(60*60));
+		$ago = "$h hours ago";
+	}
+
+	$smarty->assign('index_age', $ago);
+
+
+	#
+	# output
+	#
 
 	$smarty->display('page_index.txt');
 ?>
