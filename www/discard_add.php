fa62d1a4dd0e18dd0909110c968a44576d5729d1
Cal Henderson <cal@iamcal.com>
2012-08-10 15:32:20 -0700

    page for adding a discard rule - just allows a single field so far and does not remove existing events (just adds the filter for future events)

---
+++ www/discard_add.php
@@ -0,0 +1,77 @@
+<?
+	include('include/init.php');
+
+
+	#
+	# build a filter based on the field
+	#
+
+	$sql_filter = null;
+	$field_name = null;
+	$field_value = null;
+
+	if ($_REQUEST['user_id']){
+		$id = intval($_REQUEST['user_id']);
+
+		$sql_filter = "user_id={$id}";
+		$field_name = 'user_id';
+		$field_value = $id;
+	}
+
+	if (!strlen($sql_filter)){
+
+		die("No filter rule found");
+	}
+
+	$smarty->assign('field_name', $field_name);
+	$smarty->assign('field_value', $field_value);
+
+
+	#
+	# confirm the rule?
+	#
+
+	if ($_POST['apply']){
+
+		$note = trim($_POST['reason']);
+
+		$ret = db_insert('js_discards', array(
+			'date_added'	=> time(),
+			'is_deleted'	=> 0,
+			'field'		=> AddSlashes($field_name),
+			'value'		=> AddSlashes($field_value),
+		));
+
+		$discard_id = $ret['insert_id'];
+
+		db_insert('js_discards_notes', array(
+			'discard_id'	=> $discard_id,
+			'date_added'	=> time(),
+			'who'		=> $cfg['user'],
+			'note'		=> AddSlashes($note),
+		));
+
+		header("location: {$cfg['root_url']}discard/{$discard_id}/");
+		exit;
+	}
+	
+
+
+
+	#
+	# fetch some recent matches to show them
+	#
+
+	list($num) = db_list(db_fetch("SELECT COUNT(*) FROM js_errors_events WHERE $sql_filter"));
+
+	$ret = db_fetch("SELECT * FROM js_errors_events WHERE $sql_filter ORDER BY date_logged DESC LIMIT 50");
+
+	$smarty->assign('recent_events', $ret['rows']);
+	$smarty->assign('num_events', $num);
+
+
+	#
+	# display
+	#
+
+	$smarty->display('page_discard_add.txt');
